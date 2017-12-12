edec008511f3509073bc43c8d74840e5ac356a5b
Shen Yiming <soimy@163.com>
2017-12-12 18:37:48 +0800

    Implemented: Rectangle.contain()

--- src/geom/Rectangle.ts
+++ src/geom/Rectangle.ts
@@ -17,6 +17,11 @@ export class Rectangle {
         return (rect.x >= this.x + this.width || rect.x + rect.width <= this.x ||
                 rect.y >= this.y + this.height || rect.y + rect.height <= this.y);
     }
+    
+    public contain(rect:Rectangle):boolean {
+        return (rect.x >= this.x && rect.y >= this.y && 
+                rect.x + rect.width <= this.x + this.width && rect.y + rect.height <= this.y + this.width);
+    }
 
     public static Collide(rect1:Rectangle, rect2:Rectangle) { return rect1.collide(rect2); }
 }
