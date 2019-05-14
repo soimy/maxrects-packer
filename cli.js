6501913c5c558fd197a7c1ab108507c9d9c3a411
Shen Yiming <soimy@163.com>
2019-05-14 18:43:54 +0800

    CLI WIP2

--- cli.js
+++ cli.js
@@ -4,22 +4,57 @@ const pjson = require('./package.json');
 const MaxRectsPacker = require('./lib/maxrects_packer').MaxRectsPacker;
 const args = require('commander');
 const Jimp = require('jimp');
+const fs = require('fs');
+const path = require('path');
+const ext = ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"];
+
+let imageFiles;
 
 args
     .version('MaxRectsPacker v' + pjson.version)
     .usage('[options] <image-files/folder>')
     .arguments('<image-files/folder>')
     .description('CLI tools to packing and compositing image files into atlas using MaxRects packing algorithm')
-    .option('-o, --output', 'output atlas filename (Default: sprite.png/xml)', 'sprite')
-    .option('    --smart-size', 'shrink atlas to the smallest possible square (Default: true)', true)
-    .option('    --pot', 'atlas size shall be power of 2 (Default: false)', false)
-    .option('    --square', 'atlas size shall be square (Default: false)', false)
-    .option('    --rot', 'allow 90-degree rotation while packing (Default: false)', false)
+    .option('-o, --output <filename>', 'output atlas filename (Default: sprite.png/xml)', 'sprite')
+    .option('-R, --recursive', 'output atlas filename (Default: sprite.png/xml)', false)
+    .option('-a, --auto-size', 'shrink atlas to the smallest possible square (Default: true)', true)
+    .option('-p, --pot', 'atlas size shall be power of 2 (Default: false)', false)
+    .option('-s, --square', 'atlas size shall be square (Default: false)', false)
+    .option('-r, --rot', 'allow 90-degree rotation while packing (Default: false)', false)
     .action((...filesOrFolder) => {
+        let inputFiles = [];
         filesOrFolder.forEach(filePath => {
-            if (typeof(filePath) !== "object") console.log(filePath);
+            if (typeof(filePath) === "object") return;
+            if (fs.statSync(filePath).isDirectory()) {
+                walkSync(filePath);
+            }
         });
     }).parse(process.argv);
 
 const options = args.opts();
 console.log(options);
+
+/**
+ * List all files in a directory recursively in a synchronous fashion.
+ *
+ * @param {String} dir
+ * @returns {IterableIterator<String>}
+ */
+function *walkSync(dir, recursive = false) {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+        const pathToFile = path.join(dir, file);
+        const isDirectory = fs.statSync(pathToFile).isDirectory();
+        if (isDirectory && recursive) {
+            yield *walkSync(pathToFile, recursive);
+        } else {
+            yield pathToFile;
+        }
+    }
+}
+
+function imageValidate(filename) {
+    if(fs.statSync(filePath).isFile() && ext.includes(path.extname(filename))) return true;
+    return false; 
+}
