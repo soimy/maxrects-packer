bb91d9c8c7499beff915aa3646ac780defb4a17b
Shen Yiming <soimy@163.com>
2019-05-16 00:16:16 +0800

    Fix commander.js action() wrong index of arguments

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 
 const pjson = require('./package.json');
 const MaxRectsPacker = require('./lib/maxrects_packer').MaxRectsPacker;
-const args = require('commander');
+const commander = require('commander');
 const Jimp = require('jimp');
 const fs = require('fs');
 const path = require('path');
@@ -11,17 +11,22 @@ const ext = ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"];
 
 let imageFiles = [];
 
-args
+const cli = new commander.Command();
+cli
     .version('MaxRectsPacker v' + pjson.version)
     .usage('[options] <image-files/folder>')
     .arguments('<image-files/folder>')
     .description('CLI tools to packing and compositing image files into atlas using MaxRects packing algorithm')
     .option('-o, --output <filename>', 'output atlas filename (Default: sprite.png/xml)', 'sprite')
+    .option('-m, --size', 'ouput texture atlas size (defaut: 2048,2048)', (v) => {return v.split(',')}, [2048, 2048])
     .option('-R, --recursive', 'output atlas filename (Default: sprite.png/xml)', false)
     .option('-a, --auto-size', 'shrink atlas to the smallest possible square (Default: true)', true)
     .option('-p, --pot', 'atlas size shall be power of 2 (Default: true)', true)
     .option('-s, --square', 'atlas size shall be square (Default: false)', false)
     .option('-r, --rot', 'allow 90-degree rotation while packing (Default: false)', false)
+
+cli
+    .command("*")
     .action((...filesOrFolder) => {
         let inputFiles = [];
         filesOrFolder.forEach(filePath => {
@@ -39,12 +44,14 @@ args
             }
         }
         console.log("Total " + imageFiles.length + " files added.");
-    }).parse(process.argv);
+    })
+
+cli.parse(process.argv);
 
 //
 //  Initialize options
 //
-let options = args.opts();
+let options = cli.opts();
 if (!imageFiles) {
     args.outputHelp();
     process.exit(1);
@@ -58,6 +65,7 @@ options.autoSize = utils.valueQueue([options.autoSize, true]);
 options.pot = utils.valueQueue([options.pot, true]);
 options.square = utils.valueQueue([options.square, false]);
 options.rot = utils.valueQueue([options.rot, false]);
+options.size = utils.valueQueue([options.size, [2048, 2048]]);
 
 //
 // Display options
@@ -70,3 +78,7 @@ keys.forEach(key => {
     console.log(utils.pad(key, padding) + ": " + options[key]);
 });
 console.log("========================================");
+
+//
+// Load images into Rectangle objects
+//
