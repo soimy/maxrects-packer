7f39659a763266d3944b77252a9748f1014bae45
Shen Yiming <soimy@163.com>
2019-06-14 11:51:15 +0800

    chore: Remove compiled js/d.ts from git repo

--- lib/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Rectangle, IRectangle } from "./geom/Rectangle";
-export { MaxRectsPacker, IOption } from "./maxrects-packer";
-export { Bin } from "./abstract-bin";
-export { MaxRectsBin } from "./maxrects-bin";
-export { OversizedElementBin } from "./oversized-element-bin";
