aaa40a4f00b9831c96a19062863e8f773a854a1f
Shen Yiming <soimy@163.com>
2019-06-30 16:23:07 +0800

    fix: Export IBin interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Rectangle, IRectangle } from "./geom/Rectangle";
 export { MaxRectsPacker, IOption } from "./maxrects-packer";
-export { Bin } from "./abstract-bin";
+export { Bin, IBin } from "./abstract-bin";
 export { MaxRectsBin } from "./maxrects-bin";
 export { OversizedElementBin } from "./oversized-element-bin";
